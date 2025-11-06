32e2d9d0c94ba8320744221b0690d8c495a6e4c1
bggRGjQaUbCoE <githubaccount56556@proton.me>
2025-11-06 12:12:32 +0800

    feat: video download
    Signed-off-by: bggRGjQaUbCoE <githubaccount56556@proton.me>

--- lib/utils/storage_key.dart
+++ lib/utils/storage_key.dart
@@ -145,7 +145,8 @@ abstract class SettingBoxKey {
       showMemberShop = 'showMemberShop',
       enablePlayAll = 'enablePlayAll',
       enableTapDm = 'enableTapDm',
-      setSystemBrightness = 'setSystemBrightness';
+      setSystemBrightness = 'setSystemBrightness',
+      downloadPath = 'downloadPath';
 
   static const String minimizeOnExit = 'minimizeOnExit',
       windowSize = 'windowSize',
