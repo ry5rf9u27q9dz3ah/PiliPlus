f4f0db6055381b430f4e2f32348801b396d9685d
bggRGjQaUbCoE <githubaccount56556@proton.me>
2025-11-08 10:29:35 +0800

    enable audio cdn by def
    Signed-off-by: bggRGjQaUbCoE <githubaccount56556@proton.me>

--- lib/utils/storage_pref.dart
+++ lib/utils/storage_pref.dart
@@ -576,7 +576,7 @@ abstract class Pref {
       _setting.get(SettingBoxKey.enableLog, defaultValue: true);
 
   static bool get disableAudioCDN =>
-      _setting.get(SettingBoxKey.disableAudioCDN, defaultValue: true);
+      _setting.get(SettingBoxKey.disableAudioCDN, defaultValue: false);
 
   static int get minDurationForRcmd =>
       _setting.get(SettingBoxKey.minDurationForRcmd, defaultValue: 0);
