857109bf8bb4def4d335273f0d4d265d623413d2
bggRGjQaUbCoE <githubaccount56556@proton.me>
2025-11-09 11:44:04 +0800

    set system brightness
    Signed-off-by: bggRGjQaUbCoE <githubaccount56556@proton.me>

--- lib/utils/storage_pref.dart
+++ lib/utils/storage_pref.dart
@@ -872,4 +872,7 @@ abstract class Pref {
 
   static bool get showTrayIcon =>
       _setting.get(SettingBoxKey.showTrayIcon, defaultValue: true);
+
+  static bool get setSystemBrightness =>
+      _setting.get(SettingBoxKey.setSystemBrightness, defaultValue: false);
 }
